1e7b75576a2674efd56911a23f678a62fccfd571
Nirvana <straystxr@gmail.com>
2026-04-15 14:21:53 +0200

    Added Leaflet Map
    + Added Leaflet Map which works but need to customize it to our app's needs and to stylize it to fit the entire page

--- FlashPointApp/src/app/tab1/tab1.page.ts
+++ FlashPointApp/src/app/tab1/tab1.page.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+//importing leaflet library
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-tab1',
@@ -8,6 +10,28 @@ import { ExploreContainerComponent } from '../explore-container/explore-containe
   styleUrls: ['tab1.page.scss'],
   imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
 })
-export class Tab1Page {
+export class Tab1Page{
+
+  //initializing the variable 
+  map!: L.Map;
+
   constructor() {}
+
+  ngAfterViewInit(){
+    this.map = L.map('map',{
+      //adding the coordinates of where the map should be
+      center: [25.3791924, 55.4765436],
+      zoom: 15, //amount of zoom
+      renderer: L.canvas()
+    });
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(this.map);
+
+  //Marker for now no
+  //   L.marker([51.5, -0.09]).addTo(map)
+  //       .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+  //       .openPopup();
+  }
 }
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # API_Systems_Nirvana_Vella
 This repository will be used for all the API Systems unit assignments
+
+
+# Source
+## Map Intergration 
+Video 1: https://www.youtube.com/watch?v=L-izDYEeJmA
+Video 2: https://www.youtube.com/watch?v=ls_Eue1xUtY&list=PLyWyQBSWLw1NH1wsA0wkSMTlQ45P0AqCj&index=1
