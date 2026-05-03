d2eb883b9c814fb2f650370fd8102efbd9033416
Nirvana <straystxr@gmail.com>
2026-05-03 18:33:03 +0200

    Cleaned up code + Border Attempt
    Cleaned up the code as originally there would be weird grey fragments all over the map, this is because it was trying to load everything all at once, now it will load only in private initMap()
    
    I also attempted to add the border using leaflet which is why the @types/turf was installed but it came out awkward and visually unappealing
    
    Zoom controls were edited as they were coming out under the header which makes it inaccessible, still does not work but both the .ts file and .scss file were edited to try and prevent this

--- FlashPointApp/src/app/tab1/tab1.page.ts
+++ FlashPointApp/src/app/tab1/tab1.page.ts
@@ -10,43 +10,42 @@ import * as L from 'leaflet';
   styleUrls: ['tab1.page.scss'],
   imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
 })
-export class Tab1Page implements AfterViewInit{
-
+export class Tab1Page implements AfterViewInit {
   //initializing the variable 
+  //! means the map variable will be assigned immediately
   map!: L.Map;
 
   constructor() {}
 
-  ngAfterViewInit(){
-    this.map = L.map('map',{
-      //adding the coordinates of where the map should be
-      center: [35.8983, 14.5126],
-      zoom: 30, //amount of zoom
-      renderer: L.canvas(),
-      //removing the plus and minus buttons
-      zoomControl: false
-    });
-
-  //utilising a map without the landmark icons etc to make it less busy 
-  L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-      subdomains: 'abcd',
-      maxZoom: 15
-    }).addTo(this.map);
-
-  //Marker for now no
-  //   L.marker([51.5, -0.09]).addTo(map)
-  //       .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-  //       .openPopup();
+  ngAfterViewInit() {
+    this.initMap();
   }
+
   //ionic uses dynamic layout which can break the leaflet plugin
   ionViewDidEnter() {
     setTimeout(() => {
       this.map.invalidateSize();
     }, 200);
+  }
 
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [35.9375, 14.3754],
+      zoom: 11,
+      renderer: L.canvas(),
+      zoomControl: false,
+      minZoom: 10,
+      maxZoom: 20,
+    });
 
-    //bounding a map view to a specific geographical bounds
-    //fitBounds() or setMaxBounds()
-}
+    //changing the tile layer to a cleaner map
+    L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+      subdomains: 'abcd',
+      maxZoom: 20,
+    }).addTo(this.map);
+
+    //moving the zoom controls to the top right
+    L.control.zoom({position: 'topright'}).addTo(this.map);
+  }
 }
