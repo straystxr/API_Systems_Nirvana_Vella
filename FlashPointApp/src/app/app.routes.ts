760f804c093fbe6ab5d67562e8323c0b9058f268
Nirvana <straystxr@gmail.com>
2026-05-05 18:47:45 +0200

    Created login page with functionality
    + rerouted the pages so that when the user opens up the application they will immediately see the login page
    + login page designed after Figma designs (will upload the designs later)
    + When loading there will be an indicator to allow better user experience
    
    - Database connection to be added
    - Authentication to be added as well
    - Links to be done (Registration page)

--- FlashPointApp/src/app/app.routes.ts
+++ FlashPointApp/src/app/app.routes.ts
@@ -3,6 +3,15 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.page').then(m => m.LoginPage)
+  },
+  {
+    path: 'tabs',
     loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
   },
 ];
